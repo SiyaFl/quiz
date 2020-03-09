97d947f85511c171e1b67c3e3e97000769b94743
Your Name <you@example.com>
2020-03-09 13:00:40 +0200

    first commit

---
+++ coverpage.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/coverpage.css">
+    
+    <title>Main page</title>
+  </head>
+  <body>
+  <div class="container">
+  <div class="jumbotron">
+  <h1 class="display-4">Can you speak South African slang?</h1>
+  <hr class="my-4">
+  <a class="btn btn-dark btn-lg learnMore" href="index.php" role="button">Click to Continue</a>
+</div>
+</div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" >
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" >
+  </body>
+  </div>
+</html>
